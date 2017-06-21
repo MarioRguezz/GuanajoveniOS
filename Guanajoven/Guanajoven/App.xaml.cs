1c7b1924ebbe8b9ef4432f847dd693c2b3e2a3c4
I <obc993@gmail.com>
2017-06-21 01:18:57 -0500

    QR

--- Guanajoven/Guanajoven/App.xaml.cs
+++ Guanajoven/Guanajoven/App.xaml.cs
@@ -36,6 +36,9 @@ namespace Guanajoven
 				}*/
 
 
+			MainPage = new NavigationPage(new GuanajovenCodePage());
+			return;
+
 			if (PropertiesManager.IsFirstTime())
 			{
 				MainPage = new WelcomePage();
--- Guanajoven/Guanajoven/ContentPages/GuanajovenCode/GuanajovenCodePage.xaml.cs
+++ Guanajoven/Guanajoven/ContentPages/GuanajovenCode/GuanajovenCodePage.xaml.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.IO;
 using Xamarin.Forms;
+using ZXing;
 
 namespace Guanajoven
 {
+
+	public interface IBarcodeService
+	{
+		Stream ConvertImageStream(string text, int width = 500, int height = 500);
+	}
+
 	public partial class GuanajovenCodePage : ContentPage
 	{
 		public GuanajovenCodePage()
 		{
 			InitializeComponent();
+
+			var stream = DependencyService.Get<IBarcodeService>().ConvertImageStream("http://www.google.com.mx");
+			_qrImage.Source = ImageSource.FromStream(() => { return stream; });
+
 		}
+
 	}
 }
